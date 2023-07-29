b0247db3872446523860f818411272e89c6ad393
A-Lamia <git@lamia.io>
2023-07-29 17:25:29 +1000

    feat: init commit

---
+++ lua/spotlight/config.lua
@@ -0,0 +1,5 @@
+local M = {}
+
+M.default = {}
+
+return M
---
+++ lua/spotlight/init.lua
@@ -0,0 +1,10 @@
+local M = {}
+M.config = require("spotlight.config")
+
+function M.setup(opts)
+	-- M.config = vim.tbl_deep_extend("force", M.config.default, opts or {})
+
+	local core = require("spotlight.core")
+	core.run(M.config)
+end
+return M
