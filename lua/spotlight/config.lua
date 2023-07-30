a6c425372364808b78b83f8b3c0dc7fd40e29072
A-Lamia <git@lamia.io>
2023-07-30 23:11:53 +1000

    feat(config): move highlights over to config

--- lua/spotlight/config.lua
+++ lua/spotlight/config.lua
@@ -1,5 +1,30 @@
 local M = {}
 
 M.default = {}
+  replaced_hl = {
+    float = {
+      "NormalFloat:SpotlightNormal",
+      "FloatBorder:SpotlightBorder",
+      "FloatTitle:SpotlightTitle",
+    },
+    window = {
+      "Normal:SpotlightNormalNC",
+      "WinSeparator:SpotlightWinSeparator",
+      "CursorLine:SpotlightCursorLine",
+    },
+  },
+  restored_hl = {
+    float = {
+      "Normal:Normal",
+      "WinSeparator:WinSeparator",
+      "CursorLine:CursorLine",
+    },
+    window = {
+      "NormalFloat:NormalFloat",
+      "FloatBorder:FloatBorder",
+      "FloatTitle:Title",
+    },
+  },
+}
 
 return M
