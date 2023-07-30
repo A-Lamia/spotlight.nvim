08cc96eec2abe40191e96095a8538e02305948ec
A-Lamia <git@lamia.io>
2023-07-30 23:12:38 +1000

    feat: enable user settings

--- lua/spotlight/init.lua
+++ lua/spotlight/init.lua
@@ -2,7 +2,7 @@ local M = {}
 M.config = require "spotlight.config"
 
 function M.setup(opts)
-  -- M.config = vim.tbl_deep_extend("force", M.config.default, opts or {})
+  M.config = vim.tbl_deep_extend("force", M.config.default, opts or {})
 
   local core = require "spotlight.core"
   core.run(M.config)
