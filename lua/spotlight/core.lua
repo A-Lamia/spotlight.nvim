429fb812c9a405397156e809895f831c68c41543
A-Lamia <git@lamia.io>
2023-07-30 23:17:06 +1000

    refactor: refactor and cleanup code

--- lua/spotlight/core.lua
+++ lua/spotlight/core.lua
@@ -1,9 +1,7 @@
-local M = {}
-
-M.is_floating = false
-M.cur_win = 0
-
-M.user_hl = {}
+local M = {
+  is_floating = false,
+  cur_win = 0,
+}
 
 local function _is_floating(win_id)
   local valid_win, config = pcall(vim.api.nvim_win_get_config, win_id)
@@ -16,95 +14,56 @@ function M.run(opts)
   vim.api.nvim_create_autocmd("ColorScheme", {
     group = "spotlight",
     callback = function()
-      M.user_hl.LazyNormal = vim.api.nvim_get_hl(0, { name = "LazyNormal" })
-      M.user_hl.MasonNormal = vim.api.nvim_get_hl(0, { name = "MasonNormal" })
-
-      -- M.user_hl.TelescopePreviewBorder = vim.api.nvim_get_hl(0, { name = "TelescopePreviewBorder" })
-      -- M.user_hl.TelescopeResultsBorder = vim.api.nvim_get_hl(0, { name = "TelescopeResultsBorder" })
-      --
-      -- M.user_hl.TelescopePreviewNormal = vim.api.nvim_get_hl(0, { name = "TelescopePreviewNormal" })
-      -- M.user_hl.TelescopeResultsNormal = vim.api.nvim_get_hl(0, { name = "TelescopeResultsNormal" })
-      --
-      -- M.user_hl.TelescopePreviewTitle = vim.api.nvim_get_hl(0, { name = "TelescopePreviewTitle" })
-      -- M.user_hl.TelescopeResultsTitle = vim.api.nvim_get_hl(0, { name = "TelescopeResultsTitle" })
     end,
   })
-
   vim.api.nvim_create_autocmd({ "WinEnter" }, {
     group = "spotlight",
     callback = function()
-      local cur_win = vim.api.nvim_get_current_win()
-      local win_id_list = vim.api.nvim_list_wins()
-      local is_floating = _is_floating(cur_win)
-
-      if is_floating then
-        M.set(cur_win, win_id_list)
-        M.is_floating = is_floating
-      else
-        M.restore(win_id_list)
-      end
+      M.on_window_enter(opts)
     end,
   })
 end
 
-function M.set(cur_win, win_id_list)
+function M.on_window_enter(opts)
+  local cur_win = vim.api.nvim_get_current_win()
+  local win_id_list = vim.api.nvim_list_wins()
+  local is_floating = _is_floating(cur_win)
+
+  if is_floating then
+    M.apply_hl(cur_win, win_id_list, opts)
+
+    M.is_floating = is_floating
+    M.cur_win = cur_win
+  else
+    M.restore_hl(win_id_list, opts)
+  end
+end
+
+function M.apply_hl(cur_win, win_id_list, opts)
   for _, win_id in ipairs(win_id_list) do
     if _is_floating(win_id) then
       vim.api.nvim_set_option_value(
         "winhl",
-        table.concat({
-          "NormalFloat:SpotlightNormal",
-          "FloatBorder:SpotlightBorder",
-          "FloatTitle:SpotlightTitle",
-        }, ","),
-        -- { scope = "global", win = win_id }
+        table.concat(opts.replaced_hl.float, ","),
         { win = win_id }
       )
 
-      vim.api.nvim_set_hl(0, "LazyNormal", { link = "SpotlightNormal" })
-      vim.api.nvim_set_hl(0, "MasonNormal", { link = "SpotlightNormal" })
     else
       vim.api.nvim_set_option_value(
         "winhl",
-        table.concat({
-          "Normal:SpotlightNormalNC",
-          "WinSeparator:SpotlightWinSeparator",
-          "CursorLine:SpotlightCursorLine",
-        }, ","),
+        table.concat(opts.replaced_hl.window, ","),
         { win = win_id }
       )
     end
   end
-  M.cur_win = cur_win
-  M.data = { true, win_id_list }
 end
 
-function M.restore(win_id_list)
+function M.restore_hl(win_id_list, opts)
+  local restored = vim.list_extend(opts.restored_hl.float, opts.restored_hl.window)
   if M.is_floating then
     for _, win_id in ipairs(win_id_list) do
       if win_id ~= M.cur_win then
-        vim.api.nvim_set_option_value(
-          "winhl",
-          table.concat({
-            "Normal:Normal",
-            "WinSeparator:WinSeparator",
-            "CursorLine:CursorLine",
-          }, ","),
-          { win = win_id }
-        )
-
-        vim.api.nvim_set_option_value(
-          "winhl",
-          table.concat({
-            "NormalFloat:NormalFloat",
-            "FloatBorder:FloatBorder",
-            "FloatTitle:Title",
-          }, ","),
-          { win = win_id }
-        )
-
-        vim.api.nvim_set_hl(0, "LazyNormal", M.user_hl.LazyNormal)
-        vim.api.nvim_set_hl(0, "MasonNormal", M.user_hl.MasonNormal)
+        vim.api.nvim_set_option_value("winhl", table.concat(restored, ","), { win = win_id })
       end
     end
   end
